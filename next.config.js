0d69b5672bc706aa48afa3b0e55453e95ac83ddc
YousefOwilii <yousefowili.official@gmail.com>
2025-03-14 00:40:18 +0200

    Fix build errors and ESLint configuration

--- next.config.js
+++ next.config.js
@@ -7,17 +7,10 @@ const nextConfig = {
   },
   output: 'export',
   // For username.github.io repositories, both basePath and assetPrefix should be empty strings
-  // as the site will be served from the root domain
   basePath: '',
   assetPrefix: '',
   // Add trailingSlash to ensure proper routing with static export
   trailingSlash: true,
-  // Disable image optimization for static export
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
-  },
 };
 
 module.exports = nextConfig; 
